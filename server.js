665fdbd38dc297adc5fb5919ff7d747478d36d87
edsonchiminiec-sys <edsonchimini.ec@gmail.com>
2026-05-06 13:56:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4626,6 +4626,8 @@ function sanitizeWhatsAppText(text = "") {
 }
 
 async function sendWhatsAppMessage(to, body) {
+  const cleanBody = sanitizeWhatsAppText(body);
+
   const response = await fetch(`https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`, {
     method: "POST",
     headers: {
@@ -4636,7 +4638,7 @@ async function sendWhatsAppMessage(to, body) {
       messaging_product: "whatsapp",
       to,
       type: "text",
-      text: { body }
+      text: { body: cleanBody }
     })
   });
 
